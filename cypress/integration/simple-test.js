748c8d6bd1d4d52c302f615dc0681642f1724de0
Atulpriya Sharma <sharma.atulpriya@gmail.com>
2023-08-09 05:52:59 +0530

    Update simple-test.js

--- cypress/integration/simple-test.js
+++ cypress/integration/simple-test.js
@@ -2,6 +2,8 @@ describe("The Home Page", () => {
   it("successfully loads", () => {
     cy.visit("http://django-test.com:30001/index.html");
     cy.wait(10000);
-    cy.contains('We Love Socks!').should('be.visible');
+    cy.get('img[src="img/logo.png"][alt="WeaveSocks logo"].hidden-xs').should('be.visible');
+    cy.get('img[src="img/logo.png"]').should('have.attr', 'src').and('include', 'logo.png');
+    cy.get('img[alt="WeaveSocks logo"]').should('have.attr', 'alt', 'WeaveSocks logo');
     });
 });
